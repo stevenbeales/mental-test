97009b3418f9d1eb286f9f4df12666749fb4ed86
stevenbeales <stevenbeales@gmail.com>
2018-03-01 00:19:36 -0500

    spec refactoring to best practices

--- spec/instrument_spec.rb
+++ spec/instrument_spec.rb
@@ -1,38 +1,40 @@
 # frozen_string_literal: true
 
 RSpec.describe Instrument, type: :model do
+  subject { described_class.find_or_create_by name: AppConstants::TEST_INSTRUMENT }
+
   describe '.items' do
     it 'loads all items after initializing' do
-      ins = Instrument.find_by_name('Cesd-r')
-      expect(ins.items).not_to be_empty
+      expect(subject.items).not_to be_empty
     end
 
     it 'should have 20 items in first instrument' do
-      ins = Instrument.find_by_name('Cesd-r')
-      expect(ins.items.count).to eq(20)
+      expect(subject.items.count).to eq(20)
     end
 
     it 'should return a list of instrument names' do
-      expect(Instrument.list_tests).to eq(AppConstants::TEST_INSTRUMENT)
+      expect(described_class.list_tests).to eq(AppConstants::TEST_INSTRUMENT)
     end
   end
-end
 
-RSpec.describe Instrument do
-  describe '.name' do
+  describe '#name' do
     it 'does not save without a name' do
-      ins = Instrument.new
+      ins = described_class.new
       expect { ins.save! }.to raise_error ActiveRecord::RecordInvalid
     end
 
     it 'does not save with 1 character name' do
-      ins = Instrument.new name: 'a'
+      ins = described_class.new name: 'a'
       expect { ins.save! }.to raise_error ActiveRecord::RecordInvalid
     end
 
-    it 'does not save with a duplicate name' do
-      ins = Instrument.new name: 'Cesd-r'
+    it 'does not save with duplicate name' do
+      ins = described_class.new name: AppConstants::TEST_INSTRUMENT
       expect { ins.save! }.to raise_error ActiveRecord::RecordInvalid
     end
+  end
+
+  describe '#to_s' do
+    it { expect(subject.to_s).to eq(AppConstants::TEST_INSTRUMENT) }
   end
 end
--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -1,31 +1,31 @@
 # frozen_string_literal: true
 
 RSpec.describe Item, type: :model do
+  subject { described_class.find_or_create_by name: 'Appetite1' }
+
   describe '.choices' do
-    it 'should have 5 choices in first item' do
-      item = Item.find_by_name('Appetite1')
-      expect(item.choices.count).to eq(5)
+    it 'has 5 choices' do
+      expect(subject.choices.count).to eq(5)
     end
   end
 
   describe '.find_by_choice' do
-    it 'should have choice in first item eq 0' do
-      item = Item.find_by_name('Appetite1')
-      choice = item.find_choice_by_value('0')
+    it 'has choice eq 0' do
+      choice = subject.find_choice_by_value('0')
       expect(choice.value).to eq('0')
     end
   end
 
   describe '.to_s' do
-    it 'prints item name' do
+    it do
       item = Item.new(name: 'Time')
       expect(item.to_s).to eq(item.name) 
     end
   end
 
   describe '.find_by_name' do
-    it 'finds the item by item name' do
-      expect(Item.find_by_name('Appetite1').name).to eq('Appetite1')
+    it 'finds item by name' do
+      expect(subject.name).to eq('Appetite1')
     end
   end
 end
