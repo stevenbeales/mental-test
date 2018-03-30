4135d0b39ac2d36a1a5b25ab53cf103c70ff2e64
stevenbeales <stevenbeales@gmail.com>
2018-03-30 10:38:30 -0400

    touch sinatra.log

--- config/initializers/00_logger_initializer.rb
+++ config/initializers/00_logger_initializer.rb
@@ -1,13 +1,8 @@
 # frozen_string_literal: true
 
 require 'sinatra_logger'   
-require 'fileutils'
 
-FileUtils.touch(AppConstants::LOG_FILE)
-
-configure :development, :production do
-  enable :logging 
-  LOGGER ||= SinatraLogger::Loggers.file_logger(AppConstants::LOG_FILE) 
-  LOGGER ||= SinatraLogger::Loggers.stdout_logger 
-  use Rack::CommonLogger, LOGGER
-end 
+enable :logging 
+LOGGER ||= SinatraLogger::Loggers.file_logger(AppConstants::LOG_FILE) 
+LOGGER ||= SinatraLogger::Loggers.stdout_logger 
+use Rack::CommonLogger, LOGGER
