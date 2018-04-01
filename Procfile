8c2db52fa1ec66925a3483768c2ff2f13761d3c7
stevenbeales <stevenbeales@gmail.com>
2018-04-01 15:56:04 -0400

    add production .env

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: bundle exec puma -t 5:5 -e ${RACK_ENV:-production}
--- config/db.rb
+++ config/db.rb
@@ -3,5 +3,5 @@
 require 'yaml'
 require 'active_record'
 
-dbconfig = YAML.safe_load(File.read('config/database.yml'))
+dbconfig = YAML.load_file('config/database.yml')
 ActiveRecord::Base.establish_connection dbconfig[ENV['RACK_ENV']]
